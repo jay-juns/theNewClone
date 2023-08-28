823941d50bbd0e41c1a00e96c91ef61335afbaae
jay-juns <necrozerg2@naver.com>
2023-08-28 20:18:18 +0900

    Initial Commit

---
+++ theNewClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  theNewClone
+//
+//  Created by junseok Kim on 2023/08/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ theNewClone/theNewCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  theNewCloneApp.swift
+//  theNewClone
+//
+//  Created by junseok Kim on 2023/08/28.
+//
+
+import SwiftUI
+
+@main
+struct theNewCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
